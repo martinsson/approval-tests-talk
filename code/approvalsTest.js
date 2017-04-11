9f05e943026a5c9018d057b2bff135d1c1300b75
Johan MARTINSSON <martinsson.johan@gmail.com>
2017-04-11 14:11:57 +0200

    some example code

--- code/approvalsTest.js
+++ code/approvalsTest.js
@@ -1,7 +1,42 @@
 "use strict"
+require('approvals').mocha();
 
-describe("Message", () => {
-    it("is what it is", () => {
-        
+class MessageBody {
+
+    constructor(receipient) {
+        this.receipient = receipient
+    }
+
+    text() {
+        return "Hello " + this.receipient + "\n" +
+            "We're so excited to announce this new cool thing" + "\n" +
+            "\n" +
+            "It's actually so cool that it's completely freezing!";
+    }
+
+}
+
+class Message {
+
+    textFor(recipient) {
+        return this.bodyFor(recipient).text()
+    }
+
+    bodyFor(recipient) {
+        return new MessageBody(recipient)
+    }
+}
+
+describe("Message", function () {
+    let message = new Message()
+
+    it("is what it is", function () {
+        this.verify(message.textFor("Paul"))
     })
+
+    // it("works with objects", function () {
+    //     this.verifyAsJSON(message.bodyFor("Paul"))
+    // })
 })
+
+
